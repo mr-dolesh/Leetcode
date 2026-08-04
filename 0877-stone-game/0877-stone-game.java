7320c52a583642019fb14bce69578bbe1959c0fc
Dolesh kumar <hanubhai0007@gmail.com>
2026-08-04 23:43:50 +0530

    Time: 5 ms (36.74%), Space: 43.3 MB (39.15%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,6 +1,13 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
-        
+        int n = piles.length;
+        int[] dp = piles.clone();
+
+        for(int i=2; i<=n; i++){
+            for(int j=0; j+i-1<n; j++){
+                int z = j + i - 1;
+                dp[j] = Math.max(piles[j] - dp[j+1], piles[z] - dp[j]);
+            }
+        } return dp[0] > 0;
     }
-}
+} 
