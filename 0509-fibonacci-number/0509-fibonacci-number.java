9868e191640dfe56608f9b13cb33de6b2c10bfde
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-17 14:51:57 +0530

    Time: 8 ms (43.71%), Space: 41.7 MB (93.27%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int fib(int n) {
+        int result = 0;
+        if(n<=1){
+            return n;
+        }
+        return result = fib(n-1) + fib(n-2);
+        
+        
+    }
+}
