b0b9041fe8d08176597c0373aff89952eb595a27
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-23 17:58:38 +0530

    Time: 3 ms (58.39%), Space: 82.1 MB (50.76%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] left = new int[n];
+        int[] right = new int[n+1];
+
+        int min = prices[0];
+        for(int i=1; i<n; i++){
+            min = Math.min(min, prices[i]);
+            left[i] = Math.max(left[i-1], prices[i] - min);
+        }
+
+        int max = prices[n-1];
+        for(int i=n-2; i>=0; i--){
+            max = Math.max(max, prices[i]);
+            right[i] = Math.max(right[i+1], max - prices[i]);
+        }
+
+        int best = 0;
+        for(int i=0; i<n; i++){
+            best = Math.max(best, left[i] + right[i]);
+        }
+        return best;
+    }
+}
